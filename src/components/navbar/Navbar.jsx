59e553f0b33cdfaa909e08996b02c805a19489d8
abhaydesai01 <70378731+abhaydesai01@users.noreply.github.com>
2024-09-29 22:39:33 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,34 @@
+// import { IoChevronDownOutline } from "react-icons/io5";
+// import { RxHamburgerMenu } from "react-icons/rx";
+
+// const Navbar = ({ handleNavMenuOpen }) => {
+//   return (
+//     <nav className="sticky top-0 h-12 md:h-14 px-4 w-full shadow-md flex flex-row justify-between items-center z-[999] bg-white">
+//       <div className="flex flex-row justify-center items-center gap-x-2">
+//         <button onClick={handleNavMenuOpen} className="block lg:hidden">
+//           <RxHamburgerMenu size={24} />
+//         </button>
+//         <h1 className="text-lg font-semibold">StackIntel</h1>
+//       </div>
+//       <div className="flex flex-row justify-center items-center gap-x-2">
+//         <div className="flex flex-row items-center">
+//           <p className="border border-green-500 px-2 md:px-4 md:py-2 rounded-full">
+//             R
+//           </p>
+//           <button className="ml-1 text-green-500">
+//             <IoChevronDownOutline size={19} />
+//           </button>
+//         </div>
+//         <div>
+//           <p>Rahul</p>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
 import { IoChevronDownOutline } from "react-icons/io5";
 import { RxHamburgerMenu } from "react-icons/rx";
 
@@ -12,10 +43,12 @@ const Navbar = ({ handleNavMenuOpen }) => {
       </div>
       <div className="flex flex-row justify-center items-center gap-x-2">
         <div className="flex flex-row items-center">
-          <p className="border border-green-500 px-2 md:px-4 md:py-2 rounded-full">
+          {/* Changed border color to #2634bb */}
+          <p className="border border-[#2634bb] px-2 md:px-4 md:py-2 rounded-full">
             R
           </p>
-          <button className="ml-1 text-green-500">
+          {/* Changed text color to #2634bb */}
+          <button className="ml-1 text-[#2634bb]">
             <IoChevronDownOutline size={19} />
           </button>
         </div>
@@ -28,3 +61,4 @@ const Navbar = ({ handleNavMenuOpen }) => {
 };
 
 export default Navbar;
+
